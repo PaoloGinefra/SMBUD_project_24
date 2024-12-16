a2243142821a578d15f709e6f5e4851ab9276dba
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-12-16 03:03:45 +0100

    feat: add analysis notebook

--- conncheck.py
+++ conncheck.py
@@ -1,30 +1,78 @@
 from elasticsearch import Elasticsearch
 import random
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Connect to Bonsai Elasticsearch (replace with your Bonsai credentials)
-bonsai_url = ""
-es = Elasticsearch(bonsai_url)
+bonsai_url = os.getenv(
+    "BONSAI_URL"
+)
+print("Bonsai URL:", bonsai_url)
+es = Elasticsearch(bonsai_url,
+                   verify_certs=True)
+
+print("Connected to Bonsai Elasticsearch:", es.info())
 
 # Define the index name
 index_name = "recipeswithreviews"  # Name of your index
 
 # Random query examples
-queries = [
-    {
-        "query": {
-            "match": {
-                "RecipeCategory": "Dessert"
-            }
+# queries = [
+#     {
+#         "query": {
+#             "bool": {
+#                 "should": [
+#                     {
+#                         "match": {
+#                             "RecipeIngredientParts": {
+#                                 "query": "apple, salt",
+#                                 "operator": "or"
+#                             }
+#                         }
+#                     }
+#                 ]
+#             }
+#         },
+#         "fields": ["Name"]
+#     }
+# ]
+
+# # Select a random query from the list
+# random_query = random.choice(queries)
+
+# # Perform the search query
+# response = es.search(index=index_name, body=random_query)
+
+# # Print the response from Elasticsearch
+# print("Query Result:")
+# print(response)
+# print([a["fields"] for a in response["hits"]["hits"]])
+
+ingredients = ["apples"]
+limit = 1
+body = {
+    "query": {
+        "bool": {
+            "should": [
+                {
+                    "match":
+                        {
+                            "RecipeIngredientParts":
+                            {
+                                "query": " ".join(ingredients),
+                                "operator": "or"
+                            }
+                        }
+                }
+            ]
         }
     }
-]
+}
+result = es.search(index=index_name, body=body, size=limit)
+# print(result)
+data = [{"matchingScore": record['_score']}
+        for record in result['hits']['hits']]
 
-# Select a random query from the list
-random_query = random.choice(queries)
-
-# Perform the search query
-response = es.search(index=index_name, body=random_query)
-
-# Print the response from Elasticsearch
-print("Query Result:")
-print(response)
+print(data)
